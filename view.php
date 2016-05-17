0a00652def42eeb3e731e48520c216ccb565d35c
Jakobzzz <jakob.ahrneteg@gmail.com>
2016-05-17 15:39:58 +0200

    Added calander inteface

--- view.php
+++ view.php
@@ -313,7 +313,66 @@ EOT;
     function displayCalendar(){
         echo "
             <main>
-              Calendar
+            <div class='month'>
+            <ul>
+              <li class='prev'>&#10094;</li>
+              <li class='next'>&#10095;</li>
+              <li style='text-align:center'>
+                May<br>
+                <span style='font-size:18px'>2016</span>
+              </li>
+            </ul>
+            </div>
+
+            <ul class='weekdays'>
+            <li>Mo</li>
+            <li>Tu</li>
+            <li>We</li>
+            <li>Th</li>
+            <li>Fr</li>
+            <li>Sa</li>
+            <li>Su</li>
+            </ul>
+
+            <ul class='days'>
+            <li><span class='inactive'>25</span></li>
+            <li><span class='inactive'>26</span></li>
+            <li><span class='inactive'>27</span></li>
+            <li><span class='inactive'>28</span></li>
+            <li><span class='inactive'>29</span></li>
+            <li><span class='inactive'>30</span></li>
+            <li><span class='holiday'>1</span></li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li><span class='holiday'>8</span></li>
+            <li>9</li>
+            <li>10</li>
+            <li>11</li>
+            <li>12</li>
+            <li>13</li>
+            <li>14</li>
+            <li><span class='holiday'>15</span></li>
+            <li>16</li>
+            <li>17</li>
+            <li><span class='active'>18</span></li>
+            <li>19</li>
+            <li>20</li>
+            <li>21</li>
+            <li><span class='holiday'>22</span></li>
+            <li>23</li>
+            <li>24</li>
+            <li>25</li>
+            <li>26</li>
+            <li>27</li>
+            <li>28</li>
+            <li><span class='holiday'>29</span></li>
+            <li>30</li>
+            <li>31</li>
+            </ul>
             </main>";
     }
 
@@ -327,10 +386,6 @@ EOT;
     function displayGameHouseInfo(){
         echo "
             <main>
-
-                <h2>About</h2>
-                Some text about the game house.
-
                 <h1>About</h1>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate massa commodo ex finibus, non interdum ligula efficitur. <br> Vestibulum vel ipsum mauris.
                 Fusce sed tempor sapien, ac molestie risus. Etiam iaculis augue sed lobortis semper. <br> Proin imperdiet libero non odio laoreet lobortis.
